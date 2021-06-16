83568e3c58d895ab5cc0d84a6fe2aec47e687438
Russell <rdaswimmer@gmail.com>
2021-06-16 10:29:45 -0400

    fixed mdt and call_api

--- weather.py
+++ weather.py
@@ -7,26 +7,27 @@ import requests, csv
 from sys import argv
 
 filename, seen, lookup = argv[1], [], {}
-fieldnames = ['Business Name', 'Full Address', 'Latitude', 'Longitude', 'Date', 'Mean Daily Temperature']
+fieldnames = ['Latitude','Longitude','Date Refreshed','MDT_0', 'MDT_1', 'MDT_2',  'MDT_3', 'MDT_4', 'MDT_5', 'MDT_6']
 
 def call_api(gridId, gridX, gridY, i):
-    maxitr = 4
+    maxitr = 3
     data = requests.get('https://api.weather.gov/gridpoints/{office}/{gridX},{gridY}/forecast'.format(office = gridId, gridX = gridX, gridY = gridY)).json()
     try:
-        return [data['properties']['updated'], (data['properties']['periods'][0]['temperature'] + data['properties']['periods'][1]['temperature']) / 2]
+        return [data['properties']['updated'], (data['properties']['periods'][0]['temperature'] + data['properties']['periods'][1]['temperature'])/2, (data['properties']['periods'][2]['temperature'] + data['properties']['periods'][3]['temperature'])/2, (data['properties']['periods'][4]['temperature'] + data['properties']['periods'][5]['temperature'])/2, (data['properties']['periods'][6]['temperature'] + data['properties']['periods'][7]['temperature'])/2, (data['properties']['periods'][8]['temperature'] + data['properties']['periods'][9]['temperature'])/2, (data['properties']['periods'][10]['temperature'] + data['properties']['periods'][11]['temperature'])/2,  (data['properties']['periods'][12]['temperature'] + data['properties']['periods'][13]['temperature'])/2]
     except KeyError:
         if i < maxitr:
-            call_api(gridId, gridX,gridY, i+1)
+            return call_api(gridId, gridX,gridY, i+1)
         else:
             print("Server error, Skipping Entry")
-            return ["N/A","N/A"]
+            return ["Error", "Error", "Error", "Error", "Error", "Error", "Error", "Error"]
 
-with open("weather+{}.cvs".format(filename[:-4]),"w", encoding='utf8') as output:
+with open("weather+{}.csv".format(filename[:-4]),"w", encoding='utf8') as output:
     writer = csv.DictWriter(output, fieldnames = fieldnames)
     writer.writeheader()
     with open(filename, "r", encoding = 'utf8') as f:
         reader = csv.DictReader(f)
         for row in reader:
+            print("Entry: {} - {} - {}".format(row['Business Name'],row['Latitude'],row['Longitude']))
             # For each entry in the input, find what grid it is in and then get the weather there.
             data = requests.get("https://api.weather.gov/points/{lat},{lon}".format(lat = row['Latitude'].strip(), lon = row['Longitude'].strip())).json()
             gridX = data['properties']['gridX']
@@ -35,11 +36,9 @@ with open("weather+{}.cvs".format(filename[:-4]),"w", encoding='utf8') as output
             # Memoize the response if it has been seen before.
             if seen.count((gridX, gridY, gridId)) == 0:
                 seen.append((gridX, gridY, gridId))
-                data = requests.get('https://api.weather.gov/gridpoints/{office}/{gridX},{gridY}/forecast'.format(office = gridId, gridX = gridX, gridY = gridY)).json()
                 weather = call_api(gridId, gridX, gridY, 0)
                 lookup[str((gridX, gridY, gridId))] = weather
             else:
-                date = lookup[str((gridX, gridY, gridId))][0]
-                mdt = lookup[str((gridX, gridY, gridId))][1]
-            writer.writerow({'Business Name': row['Business Name'], 'Full Address': row['Full Address'], 'Latitude': row['Latitude'], 'Longitude': row['Longitude'], 'Date': weather[0], 'Mean Daily Temperature': weather[1]})
+                weather = lookup[str((gridX, gridY, gridId))]
+            writer.writerow({'Latitude': row['Latitude'], 'Longitude': row['Longitude'], 'Date Refreshed': weather[0], 'MDT_0': weather[1], "MDT_1": weather[2], 'MDT_2': weather[3], 'MDT_3': weather[4], "MDT_4": weather[5], 'MDT_5': weather[6], 'MDT_6': weather[7]})
 
